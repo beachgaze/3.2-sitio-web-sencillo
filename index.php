5b5f1dca2dc691d65546d964444aeef9dc694002
beachgaze <80933680+beachgaze@users.noreply.github.com>
2022-12-06 22:10:30 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+
+
+?>
+
+
+<!doctype html>
+<html lang="es">
+<head>
+    <title>LDSW</title>
+
+</head>
+
+
+<body background="fondo.gif">
+<img src="https://udg.mx/sites/default/files/styles/tablero/public/250718_udgvirtual_0.jpg">
+<center>
+
+
+</br></br></br></br>
+
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/3juyDzdN_oQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</p>
+</br></br></br></br>
+<hr>
+</br></br></br></br>
+
+
+
+<p>UNIVERSIDAD DE GUADALAJARA</br></br>
+
+Licenciatura en Desarrollo de Sistemas Web</br></br></br></br>
+
+Conceptualización de servicios en la nube</br></br>
+
+Unidad 3. Desarrollo de aplicaciones web en los servicios de la nube</br></br>
+
+3.2. Sitio Web sencillo</br></br></br></br>
+
+Jorge Alberto Velázquez Cano - 210150728
+
+
+
+
+
+</center>
+
+</body>
+
+
+</html>
+
